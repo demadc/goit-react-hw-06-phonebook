f8c75b3fdcad1886a71dcc3adfee61dc175fb34d
Demian <demyan.korotchenko@gmail.com>
2023-09-27 10:01:40 +0200

    111

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,17 +6,9 @@ export const store = configureStore({
   // reducer: { rootReducer },
 });
 
-// const initialState = {
-//   tasks: [
-//     { id: 0, text: 'Learn HTML and CSS', completed: true },
-//     { id: 1, text: 'Get good at JavaScript', completed: true },
-//     { id: 2, text: 'Master React', completed: false },
-//     { id: 3, text: 'Discover Redux', completed: false },
-//     { id: 4, text: 'Build amazing apps', completed: false },
-//   ],
-//   filters: {
-//     status: 'all',
-//   },
-// };
+const initialState = {
+  contacts: [],
+  filter: '',
+};
 
 // const addTask = createAction('tasks/AddTask');
